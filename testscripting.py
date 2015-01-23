06cf6326130560d0977b5d603483b9eac74af8fa
DangerousElectrician <DangerousElectrician@gmail.com>
2015-01-23 09:37:28 -0600

    example script from the manual

---
+++ testscripting.py
@@ -0,0 +1,13 @@
+import sys
+from pcbnew import *
+ 
+ 
+filename=sys.argv[1]
+ 
+pcb = LoadBoard(filename)
+for module in pcb.GetModules():   
+    print "* Module: %s"%module.GetReference()
+    module.GetValueObj().SetVisible(False)      # set Value as Hidden
+    module.GetReferenceObj().SetVisible(True)   # set Reference as Visible
+     
+pcb.Save("mod_"+filename) 
