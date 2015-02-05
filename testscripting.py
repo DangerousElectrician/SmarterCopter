6c036cb05ad367fd24788aec732f98ea91f36dc1
h <dangerousElectrician@gmail.com>
2015-02-04 22:53:30 -0600

    shrank all component references

--- testscripting.py
+++ testscripting.py
@@ -13,9 +13,9 @@ for module in pcb.GetModules():
 	print module.Reference().GetThickness()
 	module.Reference().SetWidth(304800)
 	module.Reference().SetHeight(304800)
-	module.Reference().SetThickness(63500)
-    #module.GetValueObj().SetVisible(False)      # set Value as Hidden
-    #module.GetReferenceObj().SetVisible(True)   # set Reference as Visible
+	module.Reference().SetThickness(76200)
 
-
-#pcb.Save("mod_"+filename) 
+	print module.Reference().GetWidth()
+	print module.Reference().GetHeight()
+	print module.Reference().GetThickness()
+pcb.Save("mod_"+filename) 
