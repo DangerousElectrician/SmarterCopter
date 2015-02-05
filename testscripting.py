371a79780851f12425a666eae421fa9f814f5b7e
h <dangerousElectrician@gmail.com>
2015-02-04 22:49:30 -0600

    figured out how to change text dimensions from script

--- testscripting.py
+++ testscripting.py
@@ -5,9 +5,17 @@ from pcbnew import *
 filename=sys.argv[1]
  
 pcb = LoadBoard(filename)
+print pcb.GetFileName()
 for module in pcb.GetModules():   
-    print "* Module: %s"%module.GetReference()
+	print "* Module: %s \t"%module.GetReference()
+	print module.Reference().GetWidth()
+	print module.Reference().GetHeight()
+	print module.Reference().GetThickness()
+	module.Reference().SetWidth(304800)
+	module.Reference().SetHeight(304800)
+	module.Reference().SetThickness(63500)
     #module.GetValueObj().SetVisible(False)      # set Value as Hidden
     #module.GetReferenceObj().SetVisible(True)   # set Reference as Visible
-     
+
+
 #pcb.Save("mod_"+filename) 
